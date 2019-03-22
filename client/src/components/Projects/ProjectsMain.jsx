40329555ae9236e92b2209c6e977b113473e93c2
Thomas Cosby <cosbyts@gmail.com>
2019-03-22 11:50:11 -0700

    Refactor projects banner to reduce GPU load

--- client/src/components/Projects/ProjectsMain.jsx
+++ client/src/components/Projects/ProjectsMain.jsx
@@ -50,10 +50,18 @@ class ProjectsMain extends React.Component {
         <div className="projects-view-section cosbytes-blue">
           {/* <i id="gear1" className="fas fa-cog large spin"></i> */}
           <div className="projects-banner">
-            <i id="gear2" className="fas fa-cog large spin-back"></i>
-            <i id="gear3" className="fas fa-cog large spin"></i>
-            <i id="gear4" className="fas fa-cog large spin-back-slow"></i>
-            <i id="gear5" className="fas fa-cog large spin-slow"></i>
+            <span style={{marginLeft: 'auto', marginRight: 'auto', transform: 'translate(-150px, 550px)', zIndex: 50}}>
+              <i id="gear2" className="fas fa-cog large"></i>
+            </span>
+            <span style={{marginLeft: 'auto', marginRight: 'auto', transform: 'translate(150px, 650px)', zIndex: 50}}>
+              <i id="gear3" className="fas fa-cog large"></i>
+            </span>
+            <span style={{transform: 'translate(100px, 800px)'}}>
+              <i id="gear4" className="fas fa-cog large"></i>
+            </span>
+            <span style={{marginLeft: 'auto', transform: 'translate(0px, 800px)'}}>
+              <i id="gear5" className="fas fa-cog large"></i>
+            </span>
             <div className="banner-container">
               <h1 className="centered white">Welcome to my Projects</h1>
             </div>
