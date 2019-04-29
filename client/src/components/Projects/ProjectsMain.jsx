a24645905225bedb4981bb8bceef8db1ad9e0742
Thomas Cosby <cosbyts@gmail.com>
2019-04-28 21:32:05 -0700

    Remove unused imports from Projects Main

--- client/src/components/Projects/ProjectsMain.jsx
+++ client/src/components/Projects/ProjectsMain.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Helmet } from "react-helmet";
-import { Link } from 'react-router-dom';
 
-import ProjectSectionScrollTarget from './ProjectSectionScrollTarget';
-import ProjectsMainProjectListItem from './ProjectsMainProjectListItem';
 import ProjectsMainProjectList from './ProjectsMainProjectList';
 import ProjectsMainSection from './ProjectsMainSection';
 import SpinningGearBanner from './SpinningGearBanner';
